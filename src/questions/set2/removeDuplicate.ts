dec1f0e5b3ae838fb5160f965fd6f524953b27d7
Amin Foroutan <aminforoutan7@gmail.com>
2024-12-15 20:11:22 +0330

    [feat]

--- src/questions/set2/removeDuplicate.ts
+++ src/questions/set2/removeDuplicate.ts
@@ -1,3 +1,12 @@
+/**
+ * 
+ * @param arr 
+ * 
+ * @example
+ * removeDuplicate([1,1,2,3,4]) : [1,2,3,4]
+ * removeDuplicate([5,1,2,3,4,5]) : [1,2,3,4,5]
+ */
+
 export function removeDuplicate(arr: number[]): number[] {
   throw new Error('Not Implemented!')
 }
