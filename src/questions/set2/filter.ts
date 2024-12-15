0e84f22987a76057184b3df5d35d03649089d11c
Amin Foroutan <aminforoutan7@gmail.com>
2024-12-15 20:47:51 +0330

    [ref]

--- src/questions/set2/filter.ts
+++ src/questions/set2/filter.ts
@@ -1,5 +1,15 @@
 type Fn = (number: number, index: number) => boolean
 
+
+/**
+ * 
+ * @param arr 
+ * @param fn 
+ * 
+ * @example
+ * filter([1,12,22,4, 9], (val, index) => val > 10): [12, 22]
+ * filter([1,2,3,4], (val, index) => index < 2): [1,2,3]
+ */
 export function filter(arr: number[], fn: Fn): number[] {
   throw new Error('Not Implemented!')
 }
